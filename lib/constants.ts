88e56086019eea58b520109489e2022a66b69341
Khyzer <gamer_x420x@proton.me>
2025-08-28 16:19:49 +0500

    payment added

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,8 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn'
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
+export const SENDER_NAME = process.env.SENDER_NAME || APP_NAME
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An amazon clone built with Next.js and MongoDB'
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
