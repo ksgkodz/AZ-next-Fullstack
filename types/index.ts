0cd4891369e5d02fa874e5e6a2fd87d710939f88
Khyzer <gamer_x420x@proton.me>
2025-08-30 15:55:33 +0500

    review added

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,14 @@
-import { ProductInputSchema, OrderItemSchema, CartSchema, UserInputSchema, UserSignInSchema, UserSignUpSchema, ShippingAddressSchema, OrderInputSchema } from '@/lib/validator'
+import { ProductInputSchema, OrderItemSchema, CartSchema, UserInputSchema, UserSignInSchema, UserSignUpSchema, ShippingAddressSchema, OrderInputSchema, ReviewInputSchema } from '@/lib/validator'
 import { z } from 'zod'
+
+export type IReviewInput = z.infer<typeof ReviewInputSchema>
+export type IReviewDetails = IReviewInput & {
+  _id: string
+  createdAt: string
+  user: {
+    name: string
+  }
+}
 
 export type IProductInput = z.infer<typeof ProductInputSchema>
 
@@ -12,9 +21,15 @@ export type IOrderInput = z.infer<typeof OrderInputSchema>
 
 
 
+
 export type Data = {
   users: IUserInput[]
   products: IProductInput[]
+  reviews: {
+    title: string
+    rating: number
+    comment: string
+  }[]
   headerMenus: {
     name: string
     href: string
